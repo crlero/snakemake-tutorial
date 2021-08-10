dd86a892d3001387df7054789b93c48bf17f41b1
Cristina Leal <cristina.lero@gmail.com>
2021-08-10 11:00:13 +0200

    sh

---
+++ handson/exe.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# conda activate tutorial
+
+# snakemake -n # dry-run
+snakemake --cores # uses all cores available, otherwise specify the number after the param cores
+
+# conda deactivate
